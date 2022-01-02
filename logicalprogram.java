5412c122cf2b7d3acf8c86a7379da6dc616b920b
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-03 00:18:47 +0530

    Added: Check prime number

--- logicalprogram.java
+++ logicalprogram.java
@@ -2,28 +2,29 @@ package day6;
 import java.util.*;
 public class logicalprogram {
 public static void main(String args[]){  
-	long number, sum=0;  
-	Scanner sc=new Scanner(System.in);         
-	System.out.print("Enter the number: ");  
-	number=sc.nextLong(); 
-	sc.close();
-	int i=1;  
-	while(i <= number/2)  
-	{  
-	if(number % i == 0)  
-	{  
-	sum = sum + i;  
-	}  
-	i++;  
-	}   
-	if(sum==number)  
-	{    
-	System.out.println(number+" is a perfect number.");  
-	} 
-	else   
-	System.out.println(number+" is not a perfect number.");   
-	}  
-	}  
+	 int m=0,flag=0;      
+	 int n=0;
+	 System.out.println("Enter a number");
+	 Scanner sc= new Scanner(System.in);
+	 n=sc.nextInt();
+	 sc.close();   
+	 m=n/2;      
+	 if(n==0||n==1)
+     {  
+	  System.out.println(n+" is not prime number");      
+	  }
+	 else{  
+	 for(int i=2;i<=m;i++){      
+	 if(n%i==0){      
+	     System.out.println(n+" is not prime number");      
+	     flag=1;      
+	     break;      
+	     }      
+	     }      
+	 if(flag==0)  { System.out.println(n+" is prime number"); }  
+} 
+}    
+}   
 		
 
 
