e4e420e073a4067ce19a04c38521de37c5369802
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-03 01:23:10 +0530

    Added: reverse a number

--- logicalprogram.java
+++ logicalprogram.java
@@ -2,29 +2,20 @@ package day6;
 import java.util.*;
 public class logicalprogram {
 public static void main(String args[]){  
-	 int m=0,flag=0;      
-	 int n=0;
-	 System.out.println("Enter a number");
-	 Scanner sc= new Scanner(System.in);
-	 n=sc.nextInt();
-	 sc.close();   
-	 m=n/2;      
-	 if(n==0||n==1)
-     {  
-	  System.out.println(n+" is not prime number");      
-	  }
-	 else{  
-	 for(int i=2;i<=m;i++){      
-	 if(n%i==0){      
-	     System.out.println(n+" is not prime number");      
-	     flag=1;      
-	     break;      
-	     }      
-	     }      
-	 if(flag==0)  { System.out.println(n+" is prime number"); }  
-} 
-}    
-}   
+	int number = 0, reverse = 0;
+	Scanner sc =new Scanner(System.in);
+	System.out.println("Enter a number");
+	number=sc.nextInt();
+	sc.close();
+	while(number != 0)   
+	{  
+	int remainder = number % 10;  
+	reverse = reverse * 10 + remainder;  
+	number = number/10;  
+	}  
+	System.out.println("The reverse of the given number is: " + reverse);  
+	}  
+	}  
 		
 
 
